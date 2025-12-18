2f766635291259ecabf7c3a5d0f16fb9bc9d451f
hammadbawany <hammad.bawany@gmail.com>
2025-12-18 16:57:53 +0500

    1

--- app/r2_client.py
+++ app/r2_client.py
@@ -1,46 +1,18 @@
-'''
-import os
-import boto3
-from botocore.config import Config
-
-def get_r2_client():
-    return boto3.client(
-        "s3",
-        endpoint_url=os.environ["R2_ENDPOINT"],
-        aws_access_key_id=os.environ["R2_ACCESS_KEY_ID"],
-        aws_secret_access_key=os.environ["R2_SECRET_ACCESS_KEY"],
-        region_name="auto",
-        config=Config(
-            signature_version="s3v4",
-            s3={"addressing_style": "path"},
-        ),
-    )
-
-
-def generate_presigned_put(key, content_type, expires=3600):
-    r2 = get_r2_client()
-
-    url = r2.generate_presigned_url(
-        ClientMethod="put_object",
-        Params={
-            "Bucket": os.environ["R2_BUCKET"],
-            "Key": key,
-            "ContentType": content_type,
-        },
-        ExpiresIn=expires,
-        HttpMethod="PUT",
-    )
-
-    return url
-'''
 import boto3
 import os
 
 def get_r2_client():
+    # Fix: Use the R2_ENDPOINT variable you already have
+    endpoint = os.environ.get('R2_ENDPOINT')
+
+    # robust check to ensure https:// is present
+    if endpoint and not endpoint.startswith("http"):
+        endpoint = f"https://{endpoint}"
+
     return boto3.client(
         's3',
-        endpoint_url=f"https://{os.environ['CLOUDFLARE_ACCOUNT_ID']}.r2.cloudflarestorage.com",
+        endpoint_url=endpoint,
         aws_access_key_id=os.environ['R2_ACCESS_KEY_ID'],
         aws_secret_access_key=os.environ['R2_SECRET_ACCESS_KEY'],
-        region_name="auto" # or 'us-east-1'
+        region_name="auto"
     )
