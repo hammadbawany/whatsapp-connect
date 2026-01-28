c393f23662f1d2815d33ff7019fff6323eaaf9fa
hammadbawany <hammad.bawany@gmail.com>
2026-01-28 16:15:24 +0500

    1

--- app/app.py
+++ app/app.py
@@ -1059,7 +1059,7 @@ if os.environ.get("ENABLE_CRON") == "true":
 
             scheduler = BackgroundScheduler()
             # Run every 5 minutes
-            scheduler.add_job(func=run_scheduled_automation, trigger="interval", minutes=4)
+            scheduler.add_job(func=run_scheduled_automation, trigger="interval", minutes=1)
             scheduler.start()
 
             # Shut down scheduler when app exits
